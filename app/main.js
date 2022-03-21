4e12fa16f3a8f8b84415e60864fc4e52fdbdeaa9
FloValdes <ftvaldes1@uc.cl>
2022-03-21 18:13:28 -0300

    feat(playGame): add welcome message

--- app/main.js
+++ app/main.js
@@ -1,1 +1,18 @@
+const _ = require('lodash');
+
 const initGame = (...args) => args.map((name) => ({ name, points: 501 }));
+
+const playerNames = (players) => players.map((player) => player.name);
+
+const stringPlayerNames = (names) => names.reduce((a, b, i, array) => a + (i < array.length - 1 ? ', ' : ' y ') + b);
+
+const playerNamesLog = _.flowRight([stringPlayerNames, playerNames]);
+
+const welcomeLog = (players) => `Juego inicializado con jugadores ${playerNamesLog(players)}.`;
+
+const playGame = (...args) => {
+  const players = initGame(...args);
+  console.log(welcomeLog(players));
+};
+
+playGame('Pepe', 'Lucía', 'María');
