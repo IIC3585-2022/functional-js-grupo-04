e130acaa76da08bee429f993dce46a2a4da643f4
simontoya <simontoya@uc.cl>
2022-03-27 20:04:12 -0300

    feat(gameSimulation): add game simulation and game logs

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const prompt = require('prompt-sync')({ sigint: true });
 
 const initGame = (...args) => args.map((name) => ({ name, points: 501 }));
 
@@ -17,21 +18,45 @@ const moveValue = (move) => {
   return move === 'SB' ? 25 : 50;
 };
 
+const applyMove = (score, move) => (score === 0 ? 0 : Math.abs(score - move));
+
 const insertMoves = (player, moves) => {
   const mapMovesToValues = moves.map((move) => moveValue(move));
-  let currentScore = player.points;
-  mapMovesToValues.some((move) => {
-    currentScore = Math.abs(currentScore - move);
-    return currentScore === 0;
-  });
-  return currentScore;
+  const currentScore = player.points;
+  console.log(currentScore);
+  const finalValue = mapMovesToValues.reduce((score, move) => applyMove(score, move), currentScore);
+  return finalValue;
 };
+
+const playerLog = (player) => `Ingrese lanzamientos de ${player.name}: `;
+
+const promptMessage = () => {
+  const playerGame = prompt('> ');
+  const cleanPlayerGame = playerGame.replace(/'/g, '"');
+  return cleanPlayerGame;
+};
+
+const parseMessage = (message) => {
+  const parsedArray = JSON.parse(message);
+  return parsedArray;
+};
+
+const currentPointsMessage = (currentPoints, player) => (currentPoints === 0 ? `${player.name} queda con 0 puntos y gana el juego. Felicitaciones ${player.name} !!` : `${player.name} queda con ${currentPoints}`);
+
+const playTurn = _.flowRight([parseMessage, promptMessage]);
 
 const playGame = (...args) => {
   const players = initGame(...args);
   console.log(welcomeLog(players));
+  while (!_.some(players, { points: 0 })) {
+    players.map((player) => {
+      console.log(playerLog(player));
+      const move = playTurn();
+      const currentPoints = insertMoves(player, move);
+      console.log(currentPointsMessage(currentPoints, player));
+      return { name: player.name, points: currentPoints };
+    });
+  }
 };
 
 playGame('Pepe', 'Lucía', 'María');
-
-console.log(insertMoves({ name: 'Felipe', points: 100 }, [[1, 50], [2, 15], [3, 15]]));
