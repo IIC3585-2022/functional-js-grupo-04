c94e7c430d9a70f507691848b7812fb28ca8941f
FelipeCampbell <fcampbell@uc.cl>
2022-03-27 18:31:29 -0300

    feat(insertMoves): function to process moves to points

--- app/main.js
+++ app/main.js
@@ -10,6 +10,31 @@ const playerNamesLog = _.flowRight([stringPlayerNames, playerNames]);
 
 const welcomeLog = (players) => `Juego inicializado con jugadores ${playerNamesLog(players)}.`;
 
+const moveValue = (move) => {
+  if (move instanceof Array) {
+    return _.multiply(...move);
+  }
+  return move === 'SB' ? 25 : 50;
+};
+
+const pointsCalculator = (totalPts, movePts) => {
+  const newScore = totalPts - movePts;
+  return newScore < 0 ? -newScore : newScore;
+};
+
+const insertMoves = (player, moves) => {
+  const mapMovesToValues = moves.map((move) => moveValue(move));
+  let currentScore = player.points;
+  mapMovesToValues.map((move) => {
+    currentScore = pointsCalculator(currentScore, move);
+    if (currentScore === 0) {
+      return 0;
+    }
+    return currentScore;
+  });
+  return currentScore;
+};
+
 const playGame = (...args) => {
   const players = initGame(...args);
   console.log(welcomeLog(players));
